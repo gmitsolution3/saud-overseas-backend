12c5ecb5d87021e82672ce258ade39275d0428a5
gmitsolution3 <gmitsolution3@gmail.com>
2026-06-10 18:21:18 +0600

    feat: updated connectDB for vercel

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
-
 import config from ".";
 
+let isConnected = false;
+
 export default async function connectDB() {
-  mongoose
-    .connect(config.mongoURI, config.databaseConfig)
-    .then(() => console.log("Database connected!"))
-    .catch((error) => console.log(`Database error: ${error}`));
+  if (isConnected) return;
+
+  await mongoose.connect(config.mongoURI, {
+    ...config.databaseConfig,
+    bufferCommands: false,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+  });
+
+  isConnected = true;
+  console.log("Database connected!");
 }
